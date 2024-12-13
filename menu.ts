e5d3ccb7d7cf35015d7f0419af737a8b4948542a
EliseuZz <100594790+EliseuZeu@users.noreply.github.com>
2024-12-13 14:22:11 -0300

    Criação da interface e heranças das classes conta

--- menu.ts
+++ menu.ts
@@ -1,11 +1,35 @@
 import readlinesync = require("readline-sync");
-import { Conta } from "./src/model/Conta";
-import { colors } from "./src/util/color"; // paleta de cores
+import { ContaCorrente } from "./src/model/ContaCorrente";
+import { ContaPoupanca } from "./src/model/ContaPoupanca";
+import { colors } from './src/util/color';
+
 
 export function main() {
     let opcao: number;
 
-    const c1 = new Conta(1, 123, 1, "Jonas", 10000);
+    // Contas Correntes
+    const cc1 = new ContaCorrente(3, 789, 1, "Andressa", 100000, 1000);
+    cc1.visualizar();
+
+    // Saque na Conta Corrente
+    cc1.sacar(100500);
+    cc1.visualizar();
+
+    // Depósito na Conta Corrente
+    cc1.depositar(2000);
+    cc1.visualizar();
+
+   // Contas Correntes
+   const cp1 = new ContaPoupanca(4, 789, 2, "Amanda", 10000, 15);
+   cp1.visualizar();
+
+   // Saque na Conta Poupança
+   cp1.sacar(500);
+   cp1.visualizar();
+
+   // Depósito na Conta Poupança
+   cp1.depositar(2000);
+   cp1.visualizar();
 
     while (true) {
         // Limpa o console antes de exibir o menu
@@ -51,39 +75,56 @@ export function main() {
         switch (opcao) {
             case 1:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Criar Conta\n${colors.reset}`);
+                keyPress()
                 break;
             case 2:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Listar todas as Contas\n${colors.reset}`);
+                keyPress()
                 break;
             case 3:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Buscar Conta por Número\n${colors.reset}`);
+                keyPress()
                 break;
             case 4:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Atualizar Dados da Conta\n${colors.reset}`);
+                keyPress()
                 break;
             case 5:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Apagar Conta\n${colors.reset}`);
+                keyPress()
                 break;
             case 6:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Realizar Saque\n${colors.reset}`);
+                keyPress()
                 break;
             case 7:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Fazer Depósito\n${colors.reset}`);
+                keyPress()
                 break;
             case 8:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Transferir Valores entre Contas\n${colors.reset}`);
+                keyPress()
                 break;
             case 9:
                 console.log(`${colors.bg.green}${colors.fg.white}\n\n👉 Visualizar Extrato\n${colors.reset}`);
+                keyPress()
                 break;
             default:
                 console.log(`${colors.bg.redbright}${colors.fg.white}\n❌ Opção Inválida! Por favor, tente novamente.\n${colors.reset}`);
+                keyPress()
                 break;
         }
 
         // Pausa antes de retornar ao menu
         readlinesync.question("\nPressione ENTER para continuar...");
     }
+}
+
+
+function keyPress(): void {
+    console.log(colors.reset, "");
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
 }
 
 /* Função com informações sobre o projeto */
